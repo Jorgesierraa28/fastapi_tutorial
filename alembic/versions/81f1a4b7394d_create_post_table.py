d3d9d075322a9e6d0a8dd6e25a73addd117a4e3d
Jorge Sierra <jorge.sierra@rappi.com>
2022-09-12 22:20:37 -0500

    alembic init

---
+++ alembic/versions/81f1a4b7394d_create_post_table.py
@@ -0,0 +1,24 @@
+"""create post table
+
+Revision ID: 81f1a4b7394d
+Revises: 
+Create Date: 2022-09-12 22:14:49.653526
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '81f1a4b7394d'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
