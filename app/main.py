2e0ce5324ecbf17af6a4ea8b87c65daf1cf85255
Jorge Sierra <jorge.sierra@rappi.com>
2022-09-20 21:48:26 -0500

    requirements.txt

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from . import models
 from .database import engine
 from .routers import posts, users, auth,vote
 from pydantic import BaseSettings
+from fastapi.middleware.cors import CORSMiddleware
 
 
 
@@ -12,6 +13,18 @@ models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI() #incicializa la API app 
+
+origins = [
+ '*'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(posts.router)
 app.include_router(users.router)
